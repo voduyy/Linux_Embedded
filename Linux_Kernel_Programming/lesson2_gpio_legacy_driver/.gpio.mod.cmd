cae89bea608a6c49762fc2518e0a807ed5968348
voduy <voduy21522015@gmail.com>
2024-06-06 20:50:16 -0700

    delete module

--- Linux_Kernel_Programming/lesson2_gpio_legacy_driver/.gpio.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/voduy/Workspace/Linux_Embedded/Linux_Kernel_Programming/lesson2_gpio_subsystem/gpio.mod := { echo  /home/voduy/Workspace/Linux_Embedded/Linux_Kernel_Programming/lesson2_gpio_subsystem/gpio.o;  echo; } > /home/voduy/Workspace/Linux_Embedded/Linux_Kernel_Programming/lesson2_gpio_subsystem/gpio.mod
